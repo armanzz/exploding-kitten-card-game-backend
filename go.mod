ea288d8fea713faa0721e26ce52ff518be1deec6
Arman Singh <118332394+armanzz@users.noreply.github.com>
2024-04-04 06:56:08 -0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/armanzz/Exploding-Kitten-Card-Game
+
+go 1.22.1
+
+require (
+	github.com/go-redis/redis/v9 v9.0.0-rc.2
+	github.com/gorilla/handlers v1.5.2
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/redis/go-redis/v9 v9.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sort"
+	"strconv"
+
+	"github.com/go-redis/redis/v9"
+	"github.com/gorilla/mux"
+)
+
+var ctx = context.Background()
+var client *redis.Client
+
+// User structure to hold username and wins
+type User struct {
+	Username string `json:"username"`
+	Wins     int    `json:"wins"`
+}
+
+func init() {
+	// Initialize the Redis client
+	client = redis.NewClient(&redis.Options{
+		Addr:     "redis-18100.c212.ap-south-1-1.ec2.cloud.redislabs.com:18100",
+		Password: "ZGYYfdHk8tb76LZjdhFzmzwRXantEL4O",
+		DB:       0,
+	})
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Register routes
+	r.HandleFunc("/api/register", registerHandler).Methods("POST")
+	r.HandleFunc("/api/win", winHandler).Methods("POST")
+	r.HandleFunc("/api/leaderboard", leaderboardHandler).Methods("GET")
+
+	// Start HTTP server
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	exists := client.Exists(ctx, user.Username).Val()
+
+	if exists == 0 {
+		// User does not exist, initialize wins to 0
+		user.Wins = 0
+
+		// Store user in Redis
+		if err := client.Set(ctx, user.Username, strconv.Itoa(user.Wins), 0).Err(); err != nil {
+			http.Error(w, "Failed to register user", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "User %s registered successfully", user.Username)
+	}
+}
+
+func winHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Fetch current wins and increment
+	Wins, err := client.Get(ctx, user.Username).Result()
+	if err == redis.Nil {
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	} else if err != nil {
+		http.Error(w, "Error fetching user data", http.StatusInternalServerError)
+		return
+	}
+
+	currentWins, err := strconv.Atoi(Wins)
+	if err != nil {
+		http.Error(w, "Error converting wins", http.StatusInternalServerError)
+		return
+	}
+
+	// Increment and update user wins in Redis
+	newWins := currentWins + 1
+	//fmt.Printf("New wins count: %d\n", newWins)
+	if err := client.Set(ctx, user.Username, strconv.Itoa(newWins), 0).Err(); err != nil {
+		http.Error(w, "Failed to update wins", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "User %s wins updated successfully", user.Username)
+}
+
+func leaderboardHandler(w http.ResponseWriter, r *http.Request) {
+	var leaderboard []User
+
+	iter := client.Scan(ctx, 0, "*", 0).Iterator()
+	for iter.Next(ctx) {
+		username := iter.Val()
+		wins, err := client.Get(ctx, username).Result()
+		if err != nil {
+			http.Error(w, "Error fetching leaderboard", http.StatusInternalServerError)
+			return
+		}
+
+		winsInt, _ := strconv.Atoi(wins)
+		user := User{Username: username, Wins: winsInt}
+		leaderboard = append(leaderboard, user)
+	}
+
+	// Sort the leaderboard based on wins
+	sort.Slice(leaderboard, func(i, j int) bool {
+		return leaderboard[i].Wins > leaderboard[j].Wins
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(leaderboard)
+}
